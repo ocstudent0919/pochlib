5345b54aec0f4c1a2ba5062d68271c26fdf13772
ocstudent0919 <ocstudent0919@protonmail.com>
2021-04-28 18:28:08 +0000

    Session Storage implemented; Poch'list presentation feature implemented

--- js/scripts.js
+++ js/scripts.js
@@ -1,39 +1,48 @@
 const NO_RESULTS_MSG = "Aucun livre n'a été trouvé.";
 const EMPTY_FIELDS_MSG = "Merci de remplir au moins un des champs proposés.";
+const NO_INFO = "Information manquante";
+const AVOID_DUPLICATES_MSG = "Vous ne pouvez ajouter deux fois le même livre !";
 
 class Book {
-  constructor(title, id, author, description, image) {
+  constructor(title, idISBN, idItem, author, description, image) {
     this.title = title;
-    this.id = id;
+    this.idISBN = idISBN;
+    this.idItem = idItem;
     this.author = author;
     this.description = description;
     this.image = image;
   }
+
   createBookPresentation(parentElt) {
-    let section, bookInfo, imgWrapper, bookInfoChildren, icon, titleElt, idElt, authorElt, descriptionElt, imageElt;
-    section = document.createElement("section");
+    const section = document.createElement("section");
     section.classList.add("result");
-    // section.id = "res-content";
-    bookInfo = document.createElement("div");
+    const bookInfo = document.createElement("div");
     bookInfo.classList.add("result__info");
-    imgWrapper = document.createElement("div");
+    const imgWrapper = document.createElement("div");
     imgWrapper.classList.add("result__img");
-    icon = document.createElement("i");
-    icon.classList.add("fas");
-    icon.classList.add("fa-bookmark");
-    titleElt = document.createElement("h3");
+    const iconBkmrk = document.createElement("i");
+    iconBkmrk.classList.add("fas");
+    iconBkmrk.classList.add("fa-bookmark");
+    const iconTrash = document.createElement("i");
+    iconTrash.classList.add("fas");
+    iconTrash.classList.add("fa-trash");
+    iconTrash.style.display = "none";
+    const titleElt = document.createElement("h3");
     titleElt.classList.add("result__info--title");
-    idElt = document.createElement("h3");
-    authorElt = document.createElement("p");
-    descriptionElt = document.createElement("p");
-    imageElt = document.createElement("img");
+    const idElt = document.createElement("h3");
+    const idHiddenElt = document.createElement("div");
+    idHiddenElt.style.display = "none";
+    const authorElt = document.createElement("p");
+    const descriptionElt = document.createElement("p");
+    const imageElt = document.createElement("img");
     imageElt.src = this.image;
     titleElt.innerHTML = "Titre : " + this.title;
-    idElt.innerHTML = "Id : " + this.id;
+    idElt.innerHTML = "Id : " + this.idISBN;
+    idHiddenElt.innerHTML = this.idItem;
     authorElt.innerHTML = "Auteur : " + this.author;
     descriptionElt.innerHTML = "Description : " + this.description;
-    bookInfoChildren = [icon, titleElt, idElt, authorElt, descriptionElt];
-    for (let child of bookInfoChildren) {
+    const bookInfoChildren = [iconBkmrk, iconTrash, titleElt, idElt, idHiddenElt, authorElt, descriptionElt];
+    for (const child of bookInfoChildren) {
       bookInfo.appendChild(child);
     }
     imgWrapper.appendChild(imageElt);
@@ -44,13 +53,13 @@ class Book {
 }
 
 function insertElement(elt) {
-  const PARENT_DIV = document.getElementById("myBooks");
-  const TARGET = document.getElementsByTagName("hr")[0];
-  PARENT_DIV.insertBefore(elt, TARGET);
+  const parentDiv = document.getElementById("myBooks");
+  const target = document.getElementsByTagName("hr")[0];
+  parentDiv.insertBefore(elt, target);
 }
 
 function createField(label, input) {
-  let field = document.createElement("div");
+  const field = document.createElement("div");
   field.classList.add("form__field");
   field.appendChild(label);
   field.appendChild(input);
@@ -58,41 +67,50 @@ function createField(label, input) {
 }
 
 function createForm() {
-  let form = document.createElement("form");
+  const form = document.createElement("form");
   form.classList.add("form");
 
-  let labelTitle = document.createElement("label");
-  let labelTitleTxt = document.createTextNode("Titre du livre");
+  const labelTitle = document.createElement("label");
+  const labelTitleTxt = document.createTextNode("Titre du livre");
   labelTitle.setAttribute("for", "intitle");
   labelTitle.appendChild(labelTitleTxt);
 
-  let title = document.createElement("input");
+  const title = document.createElement("input");
   title.setAttribute("type", "text");
   title.setAttribute("name", "intitle");
   title.setAttribute("id", "book-title");
 
-  let labelAuthor = document.createElement("label");
-  let labelAuthorTxt = document.createTextNode("Auteur");
+  const labelAuthor = document.createElement("label");
+  const labelAuthorTxt = document.createTextNode("Auteur");
   labelAuthor.setAttribute("for", "inauthor");
   labelAuthor.appendChild(labelAuthorTxt);
 
-  let author = document.createElement("input");
+  const author = document.createElement("input");
   author.setAttribute("type", "text");
   author.setAttribute("name", "inauthor");
   author.setAttribute("id", "author");
 
-  let message = createWarningMessage("empty-fields-msg", EMPTY_FIELDS_MSG);
+  const message = createWarningMessage("empty-fields-msg", EMPTY_FIELDS_MSG);
 
-  let searchBtn = document.createElement("button");
+  const searchBtn = document.createElement("button");
   searchBtn.innerHTML = "Rechercher";
   searchBtn.classList.add("btn");
-  searchBtn.setAttribute("id", "search-btn");
-  searchBtn.onclick = searchBook;
+  searchBtn.onclick = (event) => {
+    event.preventDefault();
+    searchBook();
+    title.value = "";
+    title.focus();
+    author.value = "";
+  };
 
-  let dropBtn = document.createElement("button");
+  const dropBtn = document.createElement("button");
   dropBtn.innerHTML = "Annuler";
   dropBtn.classList.add("btn", "btn--cancel");
-  dropBtn.setAttribute("id", "cancel-btn");
+  dropBtn.onclick = (event) => {
+    event.preventDefault();
+    cancelSearch(form);
+    message.style.display = "none";
+  };
 
   form.appendChild(createField(labelTitle, title));
   form.appendChild(createField(labelAuthor, author));
@@ -115,8 +133,7 @@ function displayForm() {
 }
 
 function addButton() {
-  // Creates a new button element
-  let addBookBtn = document.createElement("button");
+  const addBookBtn = document.createElement("button");
   addBookBtn.innerHTML = "Ajouter un livre";
   addBookBtn.classList.add("btn", "btn--center");
   addBookBtn.id = "addBook";
@@ -125,7 +142,7 @@ function addButton() {
 }
 
 function createWarningMessage(msgId, msg) {
-  let message = document.createElement("div");
+  const message = document.createElement("div");
   message.id = msgId;
   message.classList.add("warning-msg");
   message.innerHTML = msg;
@@ -133,17 +150,16 @@ function createWarningMessage(msgId, msg) {
   return message;
 }
 
-function createResultsPlaceholder() {
-  let resultsContainer = document.createElement("div");
+function createResultsContainer() {
+  const resultsContainer = document.createElement("div");
   resultsContainer.id = "res-output";
   resultsContainer.classList.add("res-container");
   resultsContainer.style.display = "none";
-  let resultsLine = document.createElement("hr");
-  let resultsTitle = document.createElement("h2");
-  resultsTitle.classList.add("h2--results");
+  const resultsLine = document.createElement("hr");
+  const resultsTitle = document.createElement("h2");
   resultsTitle.innerHTML = "Résultats de recherche";
-  let warningMsg = createWarningMessage("no-results-msg", NO_RESULTS_MSG);
-  let resultsGrid = document.createElement("div");
+  const warningMsg = createWarningMessage("no-results-msg", NO_RESULTS_MSG);
+  const resultsGrid = document.createElement("div");
   resultsGrid.classList.add("res-grid");
   resultsGrid.id = "list-grid";
   resultsContainer.appendChild(resultsLine);
@@ -153,9 +169,9 @@ function createResultsPlaceholder() {
   insertElement(resultsContainer);
 }
 
-function createPochlistPlacefolder() {
-  let pochlistContainer = document.getElementById("content");
-  let pochlistGrid = document.createElement("div");
+function createPochlistContainer() {
+  const pochlistContainer = document.getElementById("content");
+  const pochlistGrid = document.createElement("div");
   pochlistGrid.classList.add("res-grid");
   pochlistGrid.id = "pochlist-grid";
   pochlistContainer.appendChild(pochlistGrid);
@@ -164,15 +180,18 @@ function createPochlistPlacefolder() {
 function onload() {
   createForm();
   addButton();
-  createResultsPlaceholder();
+  createResultsContainer();
+  createPochlistContainer();
+  displayPochlist();
 }
 
 window.addEventListener("load", onload);
 
 function setBookURL() {
-  let bookURL = "https://www.googleapis.com/books/v1/volumes?q="
-  let message = document.getElementById("empty-fields-msg");
-  if(message.style.display === "block"){
+  let bookURL = "https://www.googleapis.com/books/v1/volumes?q=";
+  const resultsContainer = document.getElementById("res-output");
+  const message = document.getElementById("empty-fields-msg");
+  if (message.style.display === "block") {
     message.style.display = "none";
   }
   const form = document.forms[0];
@@ -184,6 +203,7 @@ function setBookURL() {
   if (intitle === "" && inauthor === "") {
     message.style.display = "block";
     document.getElementById("book-title").focus();
+    resultsContainer.style.display = "none";
     return null;
   }
   if (intitle === "") {
@@ -198,14 +218,75 @@ function setBookURL() {
   return bookURL
 }
 
+function toggleIcons() {
+  const icons = document.querySelectorAll("#pochlist-grid i");
+  for (const icon of icons) {
+    if (icon.className === "fas fa-bookmark" && icon.style.display !== "none") {
+      icon.style.display = "none";
+    }
+    if (icon.className === "fas fa-trash" && icon.style.display === "none") {
+      icon.style.display = "block";
+    }
+  }
+}
+
+function addIconBookmarkAction(books) {
+  for (let i = 0; i < books.length; i++) {
+    const iconBkmrk = document.getElementsByClassName("fa-bookmark")[i];
+    iconBkmrk.onclick = () => {
+      saveBook(books[i], books[i].idItem);
+    };
+  }
+}
+
+function addIconTrashAction(idItem, parentElt) {
+  const section = parentElt.lastChild;
+  const iconTrash = section.getElementsByTagName("i")[1];
+  iconTrash.addEventListener("click", () => {
+    removeBook(idItem);
+  });
+}
+
+function isInSession(idItem) {
+  if (sessionStorage.getItem(idItem) !== null) {
+    alert(AVOID_DUPLICATES_MSG);
+    return true;
+  }
+  return false;
+}
+
+function saveBook(book, idItem) {
+  const parentDiv = document.getElementById("pochlist-grid");
+  if (!isInSession(idItem)) {
+    sessionStorage.setItem(book.idItem, JSON.stringify(book));
+    book.createBookPresentation(parentDiv);
+    toggleIcons();
+    addIconTrashAction(idItem, parentDiv);
+  }
+}
+
+function removeBook(idItem) {
+  const targetElt = document.querySelectorAll("#pochlist-grid > section");
+  targetElt.forEach((elt) => {
+    const targetId = elt.querySelector(".result__info > div");
+    if (targetId.innerHTML === idItem) {
+      elt.parentNode.removeChild(elt);
+      sessionStorage.removeItem(idItem);
+    }
+  });
+}
+
 function displayResults(data, list) {
-  let book, item, title, id, author, description, image;
+  let item, title, id, idHidden, author, description, image;
+  let books = [];
+  let index = 0;
   for (item of data.items) {
     id = null;
     title = item.volumeInfo.title;
+    idHidden = item.id;
     if (item.volumeInfo.industryIdentifiers) {
       for (let i = 0; i < item.volumeInfo.industryIdentifiers.length; i++) {
-        if (item.volumeInfo.industryIdentifiers[i].type == "ISBN_13") {
+        if (item.volumeInfo.industryIdentifiers[i].type === "ISBN_13") {
           id = item.volumeInfo.industryIdentifiers[i].identifier;
         }
       }
@@ -213,9 +294,9 @@ function displayResults(data, list) {
         id = item.volumeInfo.industryIdentifiers[0].identifier;
       }
     } else {
-      id = "Information manquante";
+      id = NO_INFO;
     }
-    author = item.volumeInfo.authors ? item.volumeInfo.authors[0] : "Information manquante";
+    author = item.volumeInfo.authors ? item.volumeInfo.authors[0] : NO_INFO;
     if (item.volumeInfo.description) {
       description = item.volumeInfo.description;
       if (description.length > 200) {
@@ -226,49 +307,80 @@ function displayResults(data, list) {
         }
       }
     } else {
-      description = "Information manquante";
+      description = NO_INFO;
     }
     image = item.volumeInfo.imageLinks ? item.volumeInfo.imageLinks.thumbnail : "images/unavailable.png";
-    book = new Book(title, id, author, description, image);
-    book.createBookPresentation(list);
+    books[index] = new Book(title, id, idHidden, author, description, image);
+    books[index].createBookPresentation(list);
+    index++;
   }
+  addIconBookmarkAction(books);
 }
 
-function searchBook(event) {
-  let resultsPlaceholder = document.getElementById("res-output");
-  let listOutput = document.getElementById("list-grid");
-  let message = document.getElementById("no-results-msg");
-  let url = setBookURL();
-  event.preventDefault();
-  while(listOutput.childNodes.length > 0){
+function displayPochlist() {
+  const parentDiv = document.getElementById("pochlist-grid");
+  const keys = Object.keys(sessionStorage);
+  for (const key of keys) {
+    const sessionObject = JSON.parse(sessionStorage.getItem(key));
+    const book = new Book(sessionObject.title, sessionObject.idISBN, sessionObject.idItem, sessionObject.author, sessionObject.description, sessionObject.image);
+    book.createBookPresentation(parentDiv);
+    addIconTrashAction(sessionObject.idItem, parentDiv);
+  }
+  toggleIcons();
+}
+
+function searchBook() {
+  const resultsContainer = document.getElementById("res-output");
+  const listOutput = document.getElementById("list-grid");
+  const message = document.getElementById("no-results-msg");
+  const url = setBookURL();
+  while (listOutput.childNodes.length > 0) {
     cleanOutputList(listOutput);
   }
-  if(url){
-  fetch(url, {
-      method: "get"
-    })
-    .then(response => response.json())
-    .then(jsonData => {
-      if (jsonData.totalItems === 0) {
-        resultsPlaceholder.style.display = "block";
-        message.style.display = "block";
+  if (url) {
+    fetch(url, {
+        method: "get"
+      })
+      .then(response => response.json())
+      .then(jsonData => {
+        if (jsonData.totalItems === 0) {
+          resultsContainer.style.display = "block";
+          message.style.display = "block";
+          return false;
+        } else {
+          displayResults(jsonData, listOutput);
+          message.style.display = "none";
+          resultsContainer.style.display = "block";
+          return true;
+        }
+      })
+      .catch(err => {
+        console.error("Erreur :", err);
         return false;
-      } else {
-        displayResults(jsonData, listOutput);
-        message.style.display = "none";
-        resultsPlaceholder.style.display = "block";
-        return true;
-      }
-    })
-    .catch(err => {
-        console.error('Erreur :', err); return false;
-    })
-
+      })
   }
 }
 
-function cleanOutputList(parentElt){
-  for (let i = 0; i < parentElt.childNodes.length; i++) {
-    parentElt.removeChild(parentElt.childNodes[i]);
+function cleanOutputList(parentElt) {
+  while (parentElt.lastChild) {
+    parentElt.removeChild(parentElt.lastChild);
+  }
+}
+
+function cancelSearch(form) {
+  const parentElt = document.getElementById("list-grid");
+  const btn = document.getElementById("addBook");
+  const resContainer = document.getElementById("res-output");
+  const inputs = form.querySelectorAll("input");
+  if (parentElt) {
+    cleanOutputList(parentElt);
+  }
+  form.style.display = "none";
+  btn.style.display = "block";
+  resContainer.style.display = "none";
+  for (const input of inputs) {
+    if (input) {
+      input.value = "";
+    }
   }
 }
